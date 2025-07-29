51ba4d000baa73a874b30fa271a0e1d9ee7e97af
LAPTOP-YAMIR\Yarom <hello@yamir.nl>
2025-07-29 16:02:51 +0200

    [ADD] pausable leveltime and timers functionality

--- Assets/BuildYourOwnForest/Scripts/Managers/TimeManager.cs
+++ Assets/BuildYourOwnForest/Scripts/Managers/TimeManager.cs
@@ -1,7 +1,83 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
 
-//Manages multiple timestreams separating different functions allowing for specific interruptions.
-public class TimeManager : MonoBehaviour
-{
+//Manages game and level time allowing for specific interruptions and timers.
+public class TimeManager : Manager {
+    public static TimeManager Instance { get; private set; }
 
+    public float GameTime => Time.time;
+    public float LevelTime => levelTime;
+
+    [ReadOnly][SerializeField] private float levelTime;
+    [SerializeField] private bool isLevelTimeInterrupted;
+    private float interruptionTime;
+    [SerializeField] private List<Timer> timers = new();
+
+    private struct Timer {
+        public float StartTime;
+        public float Duration;
+        public bool UseLevelTime;
+        public Action OnComplete;
+
+        public Timer(float currentTime,float duration,bool useLevelTime,Action onComplete) {
+            this.StartTime = currentTime;
+            this.Duration = duration;
+            this.UseLevelTime = useLevelTime;
+            this.OnComplete = onComplete;
+        }
+    }
+
+
+    void Awake() {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+    }
+
+    void Update() {
+        if (!isLevelTimeInterrupted) {
+            levelTime = GameTime - interruptionTime;
+
+            for (int i = timers.Count - 1; i >= 0; i--) {
+                float currentTime = timers[i].UseLevelTime ? levelTime : GameTime;
+                if (currentTime >= timers[i].StartTime + timers[i].Duration) {
+                    timers[i].OnComplete();
+                    timers.RemoveAt(i);
+                }
+            }
+        }
+        else {
+            interruptionTime = GameTime - levelTime;
+        }
+    }
+
+    /// <summary>
+    /// Pauses the level time.
+    /// </summary>
+    public void PauseLevelTime() {
+        isLevelTimeInterrupted = true;
+    }
+
+    /// <summary>
+    /// Resumes the level time.
+    /// </summary>
+    public void ResumeLevelTime() {
+        isLevelTimeInterrupted = false;
+    }
+    /// <summary>
+    /// Start a timer.
+    /// </summary>
+    /// <param name="duration">In seconds</param>
+    /// <param name="useLevelTime">If true uses LevelTime, otherwise uses GameTime</param>
+    /// <param name="onComplete">Callback after duration</param>
+    /// <returns></returns>
+    public void StartTimer(float duration, bool useLevelTime, Action onComplete) {
+        float currentTime = useLevelTime ? levelTime : GameTime;
+        timers.Add(new Timer(currentTime, duration, useLevelTime, onComplete));
+    }
 }
